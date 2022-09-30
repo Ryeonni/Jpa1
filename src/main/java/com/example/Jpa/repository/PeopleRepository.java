ac1ead534016f86e7a834ec957e3a863e6ffe2d9
Ryeonni <aryeon5@gmail.com>
2022-09-30 13:05:17 +0900

    second

--- src/main/java/com/example/Jpa/repository/PeopleRepository.java
+++ src/main/java/com/example/Jpa/repository/PeopleRepository.java
@@ -3,5 +3,13 @@ package com.example.Jpa.repository;
 import com.example.Jpa.model.entity.People;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 public interface PeopleRepository extends JpaRepository<People, Integer> {
+
+
+    //public void select(){
+        //dsfsfd
+    //}
 }
